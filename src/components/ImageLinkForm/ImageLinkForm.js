6357d3235139204afb24f1223e7aea9df2794367
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-05-09 11:29:01 +0200

    Basic UI

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className='f3'>
+            {'Wklej link do zdjęcia, a aplikacja wykryje na nim twarz'}
+            </p> 
+            <div className='center'>
+            <div className='form center pa4 br3 shadow-5'>
+                <input className='f4 pa2 w-70 center' type='text'/>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Krzysieg, masz tyle punktow:'} 
+
+            </div>
+            <div className='white f1'>
+                {'#5'} 
+
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
