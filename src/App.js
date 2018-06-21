d0230e3355187e95382f7f300e7026948adc2989
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-06-21 17:44:29 +0200

    port fix

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://aqueous-bayou-13497.herokuapp.com:3000/image", {
+          fetch("https://aqueous-bayou-13497.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
